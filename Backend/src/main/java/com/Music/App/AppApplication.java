713ff1e8e26fb0822d210a79747642dad60a6d66
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-04 16:47:54 +0300

    Add SLF4J logging library

--- Backend/src/main/java/com/Music/App/AppApplication.java
+++ Backend/src/main/java/com/Music/App/AppApplication.java
@@ -1,13 +1,27 @@
 package com.Music.App;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
+
+
 @SpringBootApplication
+
 public class AppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		log.info("Application is starting...");
+
+		try {
+			log.debug("Attempting risky operation...");
+			int result = 10 / 0; // This will throw an exception
+		} catch (Exception e) {
+			log.error("An exception occurred: {}", e.getMessage(), e);
+		}
+
+		log.info("Application is shutting down.");
 	}
 
 }

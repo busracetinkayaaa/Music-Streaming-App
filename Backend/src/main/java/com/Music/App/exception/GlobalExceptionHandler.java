845e6558b1dbe0dbe3337e627582f0c8f45260a2
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-05 15:24:04 +0300

    feat: add global error handler

---
+++ Backend/src/main/java/com/Music/App/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.Music.App.exception;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
+    public ResponseEntity<String> handleConflict(Exception ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body("Hata: Bu kayit zaten mevcut (Unique Constraint Violation).");
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+}

c899a2c5bc33187938eec199f50a0e5ef7a33c2a
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-06 03:02:25 +0300

    Add entity listener to BaseEntity

--- Backend/src/main/java/com/Music/App/AppApplication.java
+++ Backend/src/main/java/com/Music/App/AppApplication.java
@@ -3,10 +3,8 @@ package com.Music.App;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
-
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+@EnableJpaAuditing
 @Slf4j
 @SpringBootApplication
 public class AppApplication {
--- Backend/src/main/java/com/Music/App/model/BaseEntity.java
+++ Backend/src/main/java/com/Music/App/model/BaseEntity.java
@@ -1,41 +1,24 @@
 package com.Music.App.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@Getter @Setter
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @CreatedDate
     private LocalDateTime createdAt;
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }

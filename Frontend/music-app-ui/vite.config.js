ac34fcc68c543cc033246a72e2eba51becd21478
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-09 00:56:34 +0300

    chore: setup tailwindcss and configure CSP

--- Frontend/music-app-ui/vite.config.js
+++ Frontend/music-app-ui/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
+  css: {
+    overlay: false, // CSS tarafındaki eval kullanımını durdurabilir
+  },
+  build: {
+    sourcemap: false // Build alırken eval kullanımını engeller
+  }
 })

b63aee8f43cc58eaf52756e8046cfc977a9a1c11
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-12 01:07:14 +0300

    chore: clean up default Vite styles and initialize custom structure

--- Frontend/music-app-ui/src/App.jsx
+++ Frontend/music-app-ui/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React,{ useState } from 'react';
+import Player from './Components/Player';
+import Sidebar from './Components/Sidebar';
+import Home from './Pages/Home';
+import Playlists from './Pages/Playlists';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [activePage, setActivePage] = useState('home');
+  const renderContent = () => {
+    switch (activePage) {
+      case 'home': return <Home />;
+      case 'playlists': return <Playlists />;
+      case 'search': return <div className="text-white p-10">Arama Sayfası Yapım Aşamasında</div>;
+      default: return <Home />;
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col h-screen bg-black text-white overflow-hidden">
+      <div className="flex flex-1 overflow-hidden">
+        <Sidebar setPage={setActivePage} />
+        <main className="flex-1 overflow-y-auto bg-gradient-to-b from-zinc-900 to-black p-8 mb-24">
+           {activePage === 'home' && <Home />}
+           {activePage === 'playlists' && <Playlists />}
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Player />
+    </div>
+  );
 }
 
-export default App
+export default App;

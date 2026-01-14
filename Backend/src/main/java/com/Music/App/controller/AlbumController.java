72ffcf305e12ffbc94260ccb540ada86e03b92d9
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-14 22:16:52 +0300

    fix: resolve artist mapping issue in album controller

--- Backend/src/main/java/com/Music/App/controller/AlbumController.java
+++ Backend/src/main/java/com/Music/App/controller/AlbumController.java
@@ -1,5 +1,6 @@
 package com.Music.App.controller;
 
+import com.Music.App.exception.ResourceNotFoundException;
 import com.Music.App.model.Album;
 import com.Music.App.model.Artist;
 import com.Music.App.model.Song;
@@ -7,7 +8,7 @@ import com.Music.App.service.AlbumService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import com.Music.App.repository.ArtistRepo;
 import java.util.List;
 
 @RestController
@@ -16,8 +17,13 @@ import java.util.List;
 
 public class AlbumController {
     private final AlbumService albumService;
+    private final ArtistRepo artistRepo;
     @PostMapping
     public ResponseEntity<Album> createAlbum(@RequestBody Album album){
+        if(album.getArtist()!=null &&album.getArtist().getId()!=null){
+            Artist existingArtist= artistRepo.findById(album.getArtist().getId()).orElseThrow(()-> new ResourceNotFoundException("Artist not found."));
+            album.setArtist(existingArtist);
+        }
         return ResponseEntity.ok(albumService.saveAlbum(album));
     }
     @GetMapping

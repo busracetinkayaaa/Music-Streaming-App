d5d25b8af0a3ae56011e4f43d002bddd7ff1e7f8
busracetinkayaaa <ceng.busracetinkaya@gmail.com>
2026-01-14 22:23:33 +0300

    fix: update PlaylistService to include required ID parameters

--- Frontend/music-app-ui/src/Services/playlistService.jsx
+++ Frontend/music-app-ui/src/Services/playlistService.jsx
@@ -2,10 +2,10 @@ import api from "./api.jsx";
 
 export const getAllPlaylists=()=> api.get("/playlists");
 export const addPlaylist=(name,user_id)=> api.post(`/playlists`, { name:name,user_id:user_id});
-export const deletePlaylist=()=> api.delete(`/playlists/${playlist_id}`);
-export const deleteSongFromPlaylist=()=> api.delete(`/playlists/${playlist_id}/delete-song/${song_id}`);
+export const deletePlaylist=(playlist_id)=> api.delete(`/playlists/${playlist_id}`);
+export const deleteSongFromPlaylist=(playlist_id,song_id)=> api.delete(`/playlists/${playlist_id}/delete-song/${song_id}`);
 export const addSongToPlaylist=(playlist_id,song_id)=> api.put(`/playlists/${playlist_id}/add-song/${song_id}`);
-export const getAllPlaylistsContainingSong=()=> api.get(`/playlists/song/${song_id}`,{song_id:song_id});
+export const getAllPlaylistsContainingSong=(song_id)=> api.get(`/playlists/song/${song_id}`,{song_id:song_id});
 export const getPlaylistByName=(name)=> api.get(`/playlists/name`, {params:{name:name}});
 export const getPlaylistDetails=(id)=> api.get(`/playlists/details/${id}`);
 
